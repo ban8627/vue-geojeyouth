cfc5e3df7019fb3491841112ac96d122d36e4478
banssa <ban8627@gmail.com>
2022-09-27 14:13:17 +0900

    modal change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ModalView/>
+    <ModalView />
     <more-wrap-view></more-wrap-view>
     <mb-bt></mb-bt>
     <mb-wrap-view></mb-wrap-view>
@@ -24,171 +24,167 @@
 </template>
 
 <script>
-import ModalView from '@/components/ModalView.vue';
-  import HeaderView from '@/components/HeaderView.vue';
-  import VisualView from '@/components/VisualView.vue';
-  import QuickMenuView from '@/components/QuickMenuView.vue';
-  import CommunityView from '@/components/CommunityView.vue';
-  import GalleryView from '@/components/GalleryView.vue';
-  import BannerView from '@/components/BannerView.vue';
-  import FooterView from '@/components/FooterView.vue';
-  import MoreWrapView from '@/components/MoreWrapView.vue';
-  import MbBt from '@/components/MbBt.vue';
-  import MbWrapView from '@/components/MbWrapView.vue';
+import ModalView from "@/components/ModalView.vue";
+import HeaderView from "@/components/HeaderView.vue";
+import VisualView from "@/components/VisualView.vue";
+import QuickMenuView from "@/components/QuickMenuView.vue";
+import CommunityView from "@/components/CommunityView.vue";
+import GalleryView from "@/components/GalleryView.vue";
+import BannerView from "@/components/BannerView.vue";
+import FooterView from "@/components/FooterView.vue";
+import MoreWrapView from "@/components/MoreWrapView.vue";
+import MbBt from "@/components/MbBt.vue";
+import MbWrapView from "@/components/MbWrapView.vue";
 
-  import $ from 'jquery'
-  import {
-    useStore
-  } from 'vuex'
-  import {
-    onUpdated
-  } from 'vue'
-  export default {
-    name: 'App',
-    components: {
-      ModalView,
-      HeaderView,
-      VisualView,
-      QuickMenuView,
-      CommunityView,
-      GalleryView,
-      BannerView,
-      FooterView,
-      MoreWrapView,
-      MbBt,
-      MbWrapView
-    },
-    setup() {
-      const store = useStore();
-      store.dispatch('fetchGetMenu');
-      store.dispatch('fetchGetInfo')
-      store.dispatch('fetchGetNews')
-      store.dispatch('fetchGetGallery')
-      store.dispatch('fetchGetVisual');
-      store.dispatch('fetchGetMovie');
-      onUpdated(() => {
-        $(window).resize(function () {
-          let temp = $(window).width();
-          if (temp > 1000) {
-            $('.mb-bt').removeClass('mb-bt-open');
-            $('.mb-dim').removeClass('mb-dim-open');
-            $('.mb-wrap').removeClass('mb-wrap-open');
-            $('.mb-menu>li').height(60);
-            $('.mb-mainmenu').removeClass('mb-mainmenu-open');
-          }
-        });
-      })
+import $ from "jquery";
+import { useStore } from "vuex";
+import { onUpdated } from "vue";
+export default {
+  name: "App",
+  components: {
+    ModalView,
+    HeaderView,
+    VisualView,
+    QuickMenuView,
+    CommunityView,
+    GalleryView,
+    BannerView,
+    FooterView,
+    MoreWrapView,
+    MbBt,
+    MbWrapView,
+  },
+  setup() {
+    const store = useStore();
+    store.dispatch("fetchGetMenu");
+    store.dispatch("fetchGetInfo");
+    store.dispatch("fetchGetNews");
+    store.dispatch("fetchGetGallery");
+    store.dispatch("fetchGetVisual");
+    store.dispatch("fetchGetMovie");
+    onUpdated(() => {
+      $(window).resize(function () {
+        let temp = $(window).width();
+        if (temp > 1000) {
+          $(".mb-bt").removeClass("mb-bt-open");
+          $(".mb-dim").removeClass("mb-dim-open");
+          $(".mb-wrap").removeClass("mb-wrap-open");
+          $(".mb-menu>li").height(60);
+          $(".mb-mainmenu").removeClass("mb-mainmenu-open");
+        }
+      });
+    });
 
-      return {}
-    }
-  }
+    return {};
+  },
+};
 </script>
 
 <style>
-  @charset 'utf-8';
-  @import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
-  @import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);
-  @import url(https://fonts.googleapis.com/css?family=Raleway:400,600,700,800);
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+@charset 'utf-8';
+@import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
+@import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);
+@import url(https://fonts.googleapis.com/css?family=Raleway:400,600,700,800);
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap");
 
-  @font-face {
-    font-family: 'GmarketSansLight';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansLight.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
+@font-face {
+  font-family: "GmarketSansLight";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansLight.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
 
-  @font-face {
-    font-family: 'GmarketSansMedium';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
+@font-face {
+  font-family: "GmarketSansMedium";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
 
-  @font-face {
-    font-family: 'GmarketSansBold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
+@font-face {
+  font-family: "GmarketSansBold";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
 
-  * {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-    outline-style: none;
-  }
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  outline-style: none;
+}
 
-  ul,
-  li {
-    list-style: none;
-  }
+ul,
+li {
+  list-style: none;
+}
 
-  img {
-    vertical-align: middle;
-    border: 0;
-  }
+img {
+  vertical-align: middle;
+  border: 0;
+}
 
-  a {
-    text-decoration: none;
-    color: #000;
-  }
+a {
+  text-decoration: none;
+  color: #000;
+}
 
+/* scrollbar */
+::-webkit-scrollbar {
+  width: 8px;
+  height: 5px;
+  background-color: #f7f7f7;
+  -moz-border-radius: 0px;
+  -webkit-border-radius: 0px;
+  border-radius: 0px;
+}
 
-  /* scrollbar */
-  ::-webkit-scrollbar {
-    width: 8px;
-    height: 5px;
-    background-color: #f7f7f7;
-    -moz-border-radius: 0px;
-    -webkit-border-radius: 0px;
-    border-radius: 0px;
-  }
+::-webkit-scrollbar-thumb {
+  background-color: #aaa;
+  -moz-border-radius: 2px;
+  -webkit-border-radius: 2px;
+  border-radius: 2px;
+}
 
-  ::-webkit-scrollbar-thumb {
-    background-color: #aaa;
-    -moz-border-radius: 2px;
-    -webkit-border-radius: 2px;
-    border-radius: 2px;
-  }
+/* 위쪽은 기본작업입니다. */
 
-  /* 위쪽은 기본작업입니다. */
+html {
+  font-size: 16px;
+}
 
-  html {
-    font-size: 16px;
-  }
+body {
+  font-family: "Open Sans", GmarketSansMedium, GmarketSansLight, GmarketSansBold,
+    "맑은 고딕", "돋움", Dotum, "굴림", Gulim, Sans-serif;
+  font-size: 12px;
+  font-size: 15px;
+  line-height: 1.5;
+  letter-spacing: -0.6px;
+}
 
-  body {
-    font-family: 'Open Sans', GmarketSansMedium, GmarketSansLight, GmarketSansBold, '맑은 고딕', '돋움', Dotum, '굴림', Gulim, Sans-serif;
-    font-size: 12px;
-    font-size: 15px;
-    line-height: 1.5;
-    letter-spacing: -0.6px;
-  }
+/* 공통요소(재사용할 내용) */
 
-  /* 공통요소(재사용할 내용) */
+.clearfix::after {
+  content: "";
+  position: relative;
+  display: block;
+  width: 100%;
+  clear: both;
+}
 
-  .clearfix::after {
-    content: '';
-    position: relative;
-    display: block;
-    width: 100%;
-    clear: both;
-  }
+.inner {
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  max-width: 1400px;
+}
 
-  .inner {
-    position: relative;
-    display: block;
-    margin: 0 auto;
-    max-width: 1400px;
-  }
-
-
-
-
-  /* 개별요소 */
-  .wrap {
-    position: relative;
-    display: block;
-  }
+/* 개별요소 */
+.wrap {
+  position: relative;
+  display: block;
+}
 </style>
--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -1,165 +1,148 @@
 <template>
   <div class="modal-wrap">
     <div class="modal-main">
-      <div class="modal-txt">
-        <h1><strong>본 사이트는 개인 포트폴리오용으로 제작되었습니다.</strong></h1>
-        <p>
-        사이트에 대한 리소스는 원저작자에게 있습니다.<br>
-        해당 사이트 관련 문제 사항이 있으시면 아래 메일을 통해 연락해주시면 삭제하겠습니다.
-        <span class="opti">본 사이트는 Chrome 에 최적화되어 있습니다.</span>
-        </p>
-        <a href="mailto:ban8627@gmail.com" class="mail-bt">
-          <span>ban8627@gmail.com</span>
-        </a>
-      </div>
+      <h1>
+        <strong>본 사이트는 개인 포트폴리오용으로 제작되었습니다.</strong>
+      </h1>
+      <p>
+        사이트에 대한 리소스는 원저작자에게 있습니다.<br />
+        해당 사이트 관련 문제 사항이 있으시면 아래 메일을 통해 연락해주시면
+        삭제하겠습니다.<br />
+        본 사이트는 Chrome 에 최적화되어 있습니다.
+      </p>
+      <a href="mailto:ban8627@gmail.com" class="mail-bt">
+        <span>ban8627@gmail.com</span>
+        <i class="fas fa-envelope"></i>
+      </a>
       <button class="modal-close">
-        <img src="@/assets/images/modal_close.png" alt="">
+        <i class="fas fa-times"></i>
       </button>
     </div>
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
-import {onMounted} from 'vue'
+import $ from "jquery";
+import { onMounted } from "vue";
 export default {
- setup(){
-  onMounted(()=>{
-    $('.modal-close').click(()=>{
-      $('.modal-wrap').hide();
-    })
-  })
-  return {}
- }
-}
+  setup() {
+    onMounted(() => {
+      $("html").css("overflow", "hidden");
+
+      const modalWrap = $(".modal-wrap");
+      const modalClose = $(".modal-close");
+
+      modalClose.click(() => {
+        modalWrap.stop().fadeOut(100);
+        $("html").css("overflow", "hidden");
+      });
+
+      const modalMain = $(".modal-main");
+      modalMain.click((event) => {
+        event.stopPropagation();
+      });
+      modalWrap.click(() => {
+        modalWrap.stop().fadeOut(100);
+        $("html").css("overflow", "auto");
+      });
+    });
+    return {};
+  },
+};
 </script>
 
-<style>
-    @font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-}
-body {
-  font-family: 'Pretendard-Regular';
-}
-  .modal-wrap {
-    position: fixed;
-    left:0;
-    top:0;
-    background: rgba(0, 0, 0, 0.7);
-    z-index: 99999;
-    display: block;
-    width:100vw;
-    height:100vh;
-  }
-  .hidden {
-    display: none;
-  }
-  .modal-main {
-    position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
-    border-radius: 15px;
-    box-shadow: 5px 10px 27px rgba(0, 0, 0, 0.7);
-    width:100%;
-    max-width:800px;
-    height:100%;
-    max-height:360px;
-    text-align: center;
-    background-color: #fff;
-    padding:10px;
-    overflow: hidden;
+<style scoped>
+@charset "utf-8";
 
-  }
-  .modal-main::before {
-    content: '';
-    position: absolute;
-    left:0;
-    top:0;
-    background:#fff url('@/assets/images/modal_caution.png') no-repeat;
-    background-position: center 25%;
-    display: block;
-    width:100%;
-    height:100%;
-    z-index: 0;
-    opacity:.5;
-  }
-  .modal-txt {
-    position: relative;
-    z-index: 5;
-  }
-  .modal-main h1 {
-    margin-top:65px;
-    font-size: 26px;
-    margin-bottom:30px;
-    font-weight: 900;
-    line-height:1.5;
-  }
-  .modal-main h1 strong {
+@font-face {
+  font-family: "Pretendard-Regular";
+  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+    format("woff");
+  font-weight: 400;
+  font-style: normal;
+}
 
-    font-size: 25px;
-  }
-  .modal-main p {
-    font-size: 20px;
-    font-weight: 600;
-    line-height:1.5;
-    margin-bottom:30px;
-  }
-  .modal-main .opti {
-    position: relative;
-    display: block;
-  }
-  .mail-bt {
-    position: relative;
-    display: block;
-    padding:5px 10px;
-    width:60%;
-    height:35px;
-    font-size: 20px;
-    margin:0 auto;
-    line-height:35px;
-    text-decoration: none;
-    transition: all .3s;
-  }
-  .mail-bt::before {
-    content: '';
-    position: absolute;
-    left:0;
-    top:0;
-    display: block;
-    width:100%;
-    height:100%;
-    background-color: #e3e7f2;
-    opacity:.5;
-    border-radius: 10px;
-    z-index: -1;
-  }
-  .mail-bt span {
-    color:#000;
-    font-weight: bold;
-  }
-  .modal-main .mail-bt:hover {
-    transform: scale(1.2);
-    opacity:1;
-  }
-  .modal-main .mail-bt:hover span {
-    transform: scale(1.2);
-  }
-  .modal-close {
-    position: absolute;
-    right:10px;
-    top:10px;
-    border:0;
-    font-size: 30px;
-    background-color: transparent;
-    cursor: pointer;
-    transition:all .3s;
-    z-index: 99;
-  }
-  .modal-close:hover {
-    transform: rotate(90deg);
-  }
+@font-face {
+  font-family: "GmarketSansMedium";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+.modal-wrap {
+  font-family: "Pretendard-Regular";
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 99999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.modal-close {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  display: block;
+  width: 14px;
+  height: 14px;
+  background-color: transparent;
+  border: 0;
+  color: #b7b7b7;
+  cursor: pointer;
+}
+
+.modal-main {
+  position: relative;
+  border-radius: 15px;
+  box-shadow: 5px 10px 27px rgba(0, 0, 0, 0.7);
+  width: 100%;
+  max-width: 800px;
+  height: 100%;
+  max-height: 360px;
+  text-align: center;
+  background: #fff url("@/assets/images/caution.png") no-repeat;
+  background-position: center 56px;
+  background-size: 221px 195px;
+  padding: 10px;
+}
+
+.modal-main h1 {
+  font-size: 25px;
+  font-weight: 900;
+  margin-top: 85px;
+  margin-bottom: 30px;
+}
+
+.modal-main p {
+  font-size: 20px;
+  line-height: 1.3em;
+  font-weight: 500;
+}
+
+.mail-bt {
+  position: relative;
+  margin-top: 50px;
+  display: inline-block;
+  font-size: 23px;
+  font-weight: 700;
+  color: #000;
+}
+
+.mail-bt::after {
+  position: absolute;
+  bottom: -4px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: block;
+  width: 100%;
+  height: 2px;
+  background-color: #000;
+  content: "";
+}
 </style>
